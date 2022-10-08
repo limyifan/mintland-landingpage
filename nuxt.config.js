f09fa45bc9d9eb4c9f815ec0b929b39b2b80689f
charles <limyifan1997@gmail.com>
2022-10-08 17:13:09 +0100

    1234

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
         ]
     },
+    publicPath:"/",
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
