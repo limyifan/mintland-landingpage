ef018cee89106c605fc55d2291f32bc9d385aac6
charles <limyifan1997@gmail.com>
2022-10-08 17:02:08 +0100

    12345

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,63 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'mintland',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    ssr: true,
+    devServer: {https: true},
+    target: 'static',
+    head: {
+        title: 'mintland',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''},
+            {name: 'format-detection', content: 'telephone=no'}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        'element-ui/lib/theme-chalk/index.css',
+        {
+            src: '~/assets/css/main.scss',
+            lang: 'scss'
+        },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    {
-      src: '~/assets/css/main.scss',
-      lang: 'scss'
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        '@/plugins/element-ui',
+        {
+            src: '~plugins/supabase.js',
+            ssr: true
+        },
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        transpile: [/^element-ui/],
     },
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui',
-    {
-      src: '~plugins/supabase.js',
-      ssr: true
+    publicRuntimeConfig: {
+        VUE_SUPABASE_URL: process.env.VUE_SUPABASE_URL,
+        VUE_SUPABASE_KEY: process.env.VUE_SUPABASE_KEY
     },
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    transpile: [/^element-ui/],
-  },
-  publicRuntimeConfig: {
-    VUE_SUPABASE_URL:  process.env.VUE_SUPABASE_URL,
-    VUE_SUPABASE_KEY:  process.env.VUE_SUPABASE_KEY
-  },
-  env: {
-    VUE_SUPABASE_URL:  process.env.VUE_SUPABASE_URL,
-    VUE_SUPABASE_KEY:  process.env.VUE_SUPABASE_KEY,
-    BASE_URL:  process.env.BASE_URL
-  },
-  dev: process.env.NODE_ENV === 'DEV'
+    env: {
+        VUE_SUPABASE_URL: process.env.VUE_SUPABASE_URL,
+        VUE_SUPABASE_KEY: process.env.VUE_SUPABASE_KEY,
+        BASE_URL: process.env.BASE_URL
+    },
+    dev: process.env.NODE_ENV === 'DEV'
 }
